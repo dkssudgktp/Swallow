de13ac4b60c071dc4aef72b805e385deef70946a
Hi:D <hellomydir@gmail.com>
2016-12-21 13:55:06 +0900

    Sytle Fixed

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   post '/write' => 'timeline#create'
   post '/comment' => 'timeline#comment'
 
+
   root 'pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
